50386018f9a96939594b4c4c62f0ce00bef6f554
Halanau327 <a.halanau@gmail.com>
2024-02-23 23:05:35 +0300

    add socialNetwork lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import NavBar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
+import Dialogs from './components/Dialogs/Dialogs';
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Stngs from "./components/settings/Stngs";
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <NavBar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path="/dialogs" component={Dialogs} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/news" component={News} />
+                    <Route path="/music" component={Music} />
+                    <Route path="/settings" component={Stngs} />
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
